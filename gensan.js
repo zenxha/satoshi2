5ec2d4d8312f6a2241b3521f780e4b60bda58adb
zenxha <komays5042@gmail.com>
2021-05-21 17:00:40 -0700

    coding class LMAO

---
+++ gensan.js
@@ -0,0 +1,11 @@
+var myname = "Hoshino Gen"
+var hairColor = "black"
+var age = 200
+
+
+
+if(age > 100) {
+    console.log("My age is over 100!")
+} else {
+    console.log('My age is NOT over 100!')
+}
